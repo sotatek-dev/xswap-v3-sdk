8eb84c59f8dec51bb37e6152a7bcd700d2c0a3ee
Sotatek-PhuongNguyen4 <phuong.nguyen4@sotatek.com>
2023-03-30 16:07:09 +0700

    add xswap v3

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+// export const POOL_INIT_CODE_HASH = '0xc24772465698cf5cb6165a15e3a579b6c0098ef5120815ecda4c0f1f95e02af0'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.

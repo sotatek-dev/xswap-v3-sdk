67122b1072f6081ffe38d6256ecc30366f9ff203
Sotatek-PhuongNguyen4 <phuong.nguyen4@sotatek.com>
2023-04-04 14:59:56 +0700

    move to xdc

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const FACTORY_ADDRESS = '0x5cC60dd4f07206a7994885572237d2467acbB5C2'
+export const FACTORY_ADDRESS = 'xdc365b480daaa48c9bc20550de2820a6bf1d0ab813'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xc24772465698cf5cb6165a15e3a579b6c0098ef5120815ecda4c0f1f95e02af0'
+export const POOL_INIT_CODE_HASH = '0x5dd58a93fdeafe39e58b1d19292aedf0a05b1a169b309c0d211599ead298fe23	'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.

09a6693a70174a9640052c21d4d4397c4b96a086
Sotatek-PhuongNguyen4 <phuong.nguyen4@sotatek.com>
2023-04-24 16:16:27 +0700

    mainnet 1.0.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const FACTORY_ADDRESS = '0x365b480daaa48c9bc20550de2820a6bf1d0ab813'
+export const FACTORY_ADDRESS = '0x90260a6407fe87706026c7d79af8a224035f08c8'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0x5dd58a93fdeafe39e58b1d19292aedf0a05b1a169b309c0d211599ead298fe23	'
+export const POOL_INIT_CODE_HASH = '0x5dd58a93fdeafe39e58b1d19292aedf0a05b1a169b309c0d211599ead298fe23'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
--- src/multicall.ts
+++ src/multicall.ts
@@ -1,8 +1,8 @@
 import { Interface } from '@ethersproject/abi'
-import IMulticall from '@uniswap/v3-periphery/artifacts/contracts/interfaces/IMulticall.sol/IMulticall.json'
+import IMulticallABI from './abis/xswap-interface-multicall.json'
 
 export abstract class Multicall {
-  public static INTERFACE: Interface = new Interface(IMulticall.abi)
+  public static INTERFACE: Interface = new Interface(IMulticallABI)
 
   /**
    * Cannot be constructed.
